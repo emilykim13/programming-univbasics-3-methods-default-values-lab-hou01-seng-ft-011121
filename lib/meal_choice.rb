3dc23f7b85d39f54e0245066dcc802adadc1ebbb
Emily Nguyen <emilyn0614@gmail.com>
2020-12-30 15:07:23 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,5 @@
 def meal_choice(veg1, veg2, protein = "tofu")
- meal_choice should puts "What a nutritious mean!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  "A plate of #{protein} with #{veg1} and #{veg2}."
+ puts "What a nutritious mean!"
 end
 
 
